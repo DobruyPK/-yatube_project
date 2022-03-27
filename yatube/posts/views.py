c9c803f1b2ca6324cca95cc2da52ae8d7dd217b1
Semen TR <DobruyPK@yandex.ru>
2022-03-27 19:12:55 +0500

    My_hope

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
-# Главная страница
-def index(request):    
-    return HttpResponse('Главная страница1')
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
-def group_posts(request,slug):    
-    return HttpResponse(f'group_posts-- {slug}')
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
